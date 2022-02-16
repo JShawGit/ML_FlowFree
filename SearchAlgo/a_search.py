cab053784905389e8ca6c4c593af8e32c902f46e
JShawGit <93174861+JShawGit@users.noreply.github.com>
2022-02-16 12:00:28 -0600

    added A* search template

---
+++ SearchAlgo/a_search.py
@@ -0,0 +1,15 @@
+# TODO: create search algorithm
+
+""" 
+A* Search Algorithm
+  This is an implementation of the A* search algorithm
+  to find a shortest path.
+"""
+def a_search(str):
+  print(str)
+  return None
+
+
+# This executes if this file is the main executor
+if __name__ == "__main__":
+  a_search("TODO: code this!")
