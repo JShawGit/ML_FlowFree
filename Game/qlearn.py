4681591076a501e57244e0bf192587db8b4dd6c0
JShaw <93174861+JShawGit@users.noreply.github.com>
2022-02-26 23:21:37 -0600

    Add files via upload

--- Game/qlearn.py
+++ Game/qlearn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 """ Q Learn ----------------------------
     This file contains the RL algorithm.
@@ -38,25 +39,32 @@ class Q_Learn_Agent():
 
         # board state and possible tile actions
         self.states  = ["UN", "CN", "CF"]
-        self.actions = ["L",  "R",  "U",  "D"]
+        self.actions = ["L",  "R", # x
+                        "U",  "D"  # y
+                        ]
 
-        # every tile (minus the dots) will have 4 possible actions
+        # TODO: is this good?
+        # every tile (minus the dots) will have 2-4 possible actions (edges)
         # each of these node-actions will have a q and r value
-        self.qValues = np.zeros((
-            self.game.grid_size[0] * self.game.grid_size[1] - len(self.game.colors)*2,
-            len(self.actions)
-        ))
-        self.rValues = np.zeros((
-            self.game.grid_size[0] * self.game.grid_size[1] - len(self.game.colors)*2,
-            len(self.actions)
-        ))
+        # two nodes share every node-action
+        x = self.game.grid_size[0]
+        y = self.game.grid_size[1]
+        self.num_nodes = x * y
+        self.num_edges =  x * (x-1) + y * (y-1)
+        self.qValues = np.zeros((self.num_edges/2, # x -
+                                 self.num_edges/2  # y |
+                                 ))
+        self.rValues = np.zeros((self.num_edges/2, # x -
+                                 self.num_edges/2  # y |
+                                 ))
 
         # possible rewards
         self.rewards = {
             self.states[0]: -100,
             self.states[1]: -100,
             self.states[2]:  100,
-            "move":            1
+            "move":            1,
+            "start":        -100
         }
 
         self.current_state = None
@@ -79,10 +87,47 @@ class Q_Learn_Agent():
         while not np.array_equal(self.current_node, self.game.final_position):
 
             # while the nodes equal, randomly select next node
-            next_state = self.current_state
+            next_node = self.current_node
             while np.array_equal(self.current_node, self.game.final_position):
+                # random move
+                move = random.choices([0, 1, 2, 3])
 
+                # move left
+                if move == self.states[move]:
+                    if next_node[0] == 0:
+                        continue
+                    next_node[0] = next_node[0] - 1
 
+                # move right
+                elif move == self.states[move]:
+                    if next_node[0] == self.grid_size[0] - 1:
+                        continue
+                    next_node[0] = next_node[0] + 1
+
+                # move up
+                elif move == self.states[move]:
+                    if next_node[1] == 0:
+                        continue
+                    next_node[1] = next_node[1] - 1
+
+                # move down
+                elif move == self.states[move]:
+                    if next_node[1] == self.grid_size[1] - 1:
+                        continue
+                    next_node[1] = next_node[1] + 1
+
+                # if back at starting node
+                if np.array_equal(next_node, self.game.start_position):
+                    self.set_reward(self.current_node, next_node, self.rewards["start"])
+                    return
+
+    # TODO: set reward (R) and Q values for each node-edge action
+    def set_reward(self, current_node, next_node, reward):
+
+        # x axis
+        if current_node[0] != next_node[0]:
+            if current_node[0] < next_node[0]:
+                self.rValues[0][] # TODO: uhm
 
 
 
